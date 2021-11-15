ed89eaf16914f145e1b53e31a47dc0f3eaa07cb6
KrzysztofKmiecik1975 <krzysztofkmiecik1975@gmail.com>
2021-11-15 14:02:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Holistech
+- [x] FIS
+- [x] email
+- [ ] scheduler
+- [ ] manual setup
+- [x] log history
---
+++ src/test/java/pl/kmiecik/holistech/HolistechApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kmiecik.holistech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolistechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
